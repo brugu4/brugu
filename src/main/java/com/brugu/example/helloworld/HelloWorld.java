ffdd98e58e1a202cef8c39e783108f13fa813ccd
Brugu Sirimalla <brugu4@gmail.com>
2015-10-30 14:33:29 -0500

    adding hello world

--- src/main/java/com/brugu/example/helloworld/HelloWorld.java
+++ src/main/java/com/brugu/example/helloworld/HelloWorld.java
@@ -1,7 +1,11 @@
 package main.java.com.brugu.example.helloworld;
 
 /**
- * Created by BS017551 on 10/30/2015.
+ * Created by Brugu on 10/30/2015.
  */
 public class HelloWorld {
+    public static void main(String args[])
+    {
+        System.out.println("Hello World");
+    }
 }
